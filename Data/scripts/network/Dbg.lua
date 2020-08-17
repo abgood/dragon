a97593453ea9c92c2faec047063493e6228d5cca
june <abgood@sina.com>
2020-08-17 20:55:02 +0800

    fix debug

--- Data/scripts/network/Dbg.lua
+++ Data/scripts/network/Dbg.lua
@@ -21,51 +21,31 @@ this.getHead = function()
 	return "";
 end
 
-this.INFO_MSG = function( s )
-	if (DEBUGLEVEL.INFO >= this.debugLevel) then
-		KBELuaUtil.Log(this.getHead() .. s);
-	end
-end
-
-this.DEBUG_MSG = function( s )
-	if (DEBUGLEVEL.DEBUG >= this.debugLevel) then
-		KBELuaUtil.Log(this.getHead() .. s);
-	end
-end
-
-this.WARNING_MSG = function(s)
-	if (DEBUGLEVEL.WARNING >= this.debugLevel) then
-		KBELuaUtil.LogWarning(this.getHead() .. s);
-	end
-end
-
-this.ERROR_MSG = function(s)
-	if (DEBUGLEVEL.ERROR >= this.debugLevel) then
-		KBELuaUtil.LogError(this.getHead() .. s);
-	end
-end
-
 
 --debug输出日志--
 function logDbg(str)
-    -- Dbg.DEBUG_MSG(str);
-	log:Write(LOG_DEBUG, "S_DBG: " .. str)
-end
-
---输出日志--
-function logInfo(str)
-    -- Dbg.INFO_MSG(str);
-	log:Write(LOG_INFO, "S_INFO: " .. str)
-end
-
---错误日志--
-function logError(str) 
-    -- Dbg.ERROR_MSG(str);
-    log:Write(LOG_ERROR, "S_ERR: " .. str)
+	if (DEBUGLEVEL.DEBUG >= this.debugLevel) then
+		log:Write(LOG_DEBUG, "S_DBG: " .. str)
+	end
 end
 
 --警告日志--
 function logWarn(str) 
-    -- Dbg.WARNING_MSG(str);
-    log:Write(LOG_WARNING, "S_WARN: " .. str)
+	if (DEBUGLEVEL.WARNING >= this.debugLevel) then
+		log:Write(LOG_WARNING, "S_WARN: " .. str)
+	end
+end
+
+--输出日志--
+function logInfo(str)
+	if (DEBUGLEVEL.INFO >= this.debugLevel) then
+		log:Write(LOG_INFO, "S_INFO: " .. str)
+	end
+end
+
+--错误日志--
+function logError(str) 
+	if (DEBUGLEVEL.ERROR >= this.debugLevel) then
+		log:Write(LOG_ERROR, "S_ERR: " .. str)
+	end
 end
